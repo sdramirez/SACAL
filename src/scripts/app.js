1aa4c369b78cf2995b71d06a52fe48660fe4197f
sauldavid <sauldavid38@gmail.com>
2014-05-23 16:04:00 -0700

    estructura basicamente

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,21 +7,67 @@ sacalApp.config(function($stateProvider) {
 
   $stateProvider
 
-  // .state("home", {
-  //   url: "views/main.html",
-  //   controller: 'MainCtrl'
-  // });
-
   .state("home", {
-	url: "/Home",
-	templateUrl: 'views/main.html',
-	controller: 'MainCtrl'
+    url: "/Home/:userType",
+    templateUrl: 'views/Home.html',
+    controller: 'HomeCtrl'
+  })
+  .state("login", {
+  	url: "/Login",
+  	templateUrl: 'views/Login.html',
+  	controller: 'LoginCtrl'
   })
 
 });
 
-sacalApp.controller("MainCtrl", function ($rootScope,$scope) {
-
-  $scope.hola = 'Si jalo';
+sacalApp.controller("MainCtrl", function ($rootScope,$scope,$location,$stateParams,$state) {
+  $location.path("/Login");
+  
+  $scope.isState = function(path){
+    return $state.is(path);
+  }
 
 });
+
+sacalApp.controller("LoginCtrl", function ($rootScope,$scope,$location,$stateParams) {
+
+  $scope.login = function(){
+    $location.path("/Home/Docente");
+  };
+
+});
+
+sacalApp.controller("HomeCtrl", function ($rootScope,$scope,$location,$stateParams,callToWebService) {
+  var userType = $stateParams.userType;
+  // $scope.llamado = true;
+  //  $scope.$watch("llamado", function () {
+  //   callToWebService.postCall('/Reservar', {},
+  //     function sucess(data){
+  //       debugger;
+  //       alert(1);
+  //     },
+  //     function error(data){
+  //       alert(0);
+  //     });
+  // }, true);
+  
+});
+
+sacalApp.service('callToWebService', [
+  '$http', '$state',
+  function ($http, $state) {
+    this.postCall = function (endpoint, postData, succesCB, errorCB) {
+        $http({
+            method:'POST',
+            url: endpoint,
+            data: JSON.stringify(postData),
+            headers: {"Content-Type": "application/json; charset=utf-8"}
+        })
+        .success(function (data){
+
+        })
+        .error(function (data, responseCode){
+          
+        });
+    };
+}])
